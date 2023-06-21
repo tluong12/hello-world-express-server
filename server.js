3bc20b27f479fe6a0f2a3d88fc6b086f0ab95f0f
tluong12 <tluong12@myseneca.ca>
2023-06-21 09:24:44 -0400

    first commit

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-import express from 'express';
+const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000; 
+const port = 3000; // You can change the port number if desired
 
 app.get('/', (req, res) => {
-  console.log('reponse from DL computer: ' + req.ip);  
-  res.send('reponse from DL computer: ' + req.ip)
- // res.send('Hello WEB322!');
+  res.send('Hello WEB322!');
 });
-// 192.168.189.1:3000
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
